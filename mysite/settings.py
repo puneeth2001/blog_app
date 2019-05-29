8c5c9461f3d3f61e6ec80b179bb57f5ad3105b0c
puneeth2001 <puneethchanda2001@gmail.com>
2019-05-29 20:32:48 +0530

    2nd Commit

--- mysite/settings.py
+++ mysite/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'pbd+95a8w#%f$95y@amw))rhqzxq6!-gp%mj4#+cfombraj-$y'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['127.0.0.1', '.pythonanywhere.com']
 
 
 # Application definition
@@ -37,7 +37,6 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'blog',
 ]
 
 MIDDLEWARE = [
